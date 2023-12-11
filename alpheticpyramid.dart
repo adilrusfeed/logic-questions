edd335989dcb85290e43a37a9dc50cac245c2e3d
adilrusfeed <adilrusfeed@gmail.com>
2023-12-11 14:55:10 +0530

    alphqabeticpyramic

--- alpheticpyramid.dart
+++ alpheticpyramid.dart
@@ -2,11 +2,21 @@ import 'dart:io';
 
 void main() {
   for (int i = 5; i >= 0; i--) {
-    for (int s = 0; s < 5 - i; s++) {
+    for (int space = 0; space < 5 - i; space++) {
       stdout.write(" ");
     }
     for (int j = 0; j < 2 * i - 1; j++) {
       stdout.write(i);
+    }
+    stdout.write('\n');
+  }
+
+  for (int i = 5; i >= 0; i--) {
+    for (int space = 0; space <= 5 - i; space++) {
+      stdout.write(' ');
+    }
+    for (int j = 0; j < 2 * i - 1; j++) {
+      stdout.write("*");
     }
     stdout.write('\n');
   }
